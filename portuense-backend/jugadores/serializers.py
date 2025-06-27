8a0af6a397b37492a5aff2f735cdf62739f000e9
Guillermo-Aguado <gaguado@fintivio.com>
2025-06-27 19:31:11 +0200

    Fixed GET usuarios

--- portuense-backend/jugadores/serializers.py
+++ portuense-backend/jugadores/serializers.py
@@ -7,20 +7,64 @@ class PanelSerializer(serializers.ModelSerializer):
         model = Panel
         fields = '__all__'
 class UserSerializer(serializers.ModelSerializer):
+    grupo = serializers.SerializerMethodField()
+    permisos = serializers.SerializerMethodField()
+    vistas = serializers.SerializerMethodField()
+
     groups = serializers.SlugRelatedField(
         many=True,
         slug_field='name',
-        queryset=Group.objects.all()
+        queryset=Group.objects.all(),
+        write_only=True
     )
+
     class Meta:
         model = User
-        fields= ['id', 'username', 'password', 'groups']
+        fields = [
+            'id',
+            'username',
+            'password',
+            'grupo',
+            'groups',
+            'permisos',
+            'vistas'
+        ]
         extra_kwargs = {'password': {'write_only': True}}
-        def create(self, validated_data):
-            groups= validated_data.pop('groups', [])
-            user = User.objects.create_user(**validated_data)
-            user.groups.set(groups)
-            return user
+
+    def get_grupo(self, obj):
+        return obj.groups.first().name if obj.groups.exists() else ""
+
+    def get_permisos(self, obj):
+        return list(obj.permisos.values('categoria', 'equipo'))
+
+    def get_vistas(self, obj):
+        return list(obj.vistas.values_list('vista', flat=True))
+
+    def create(self, validated_data):
+        groups = validated_data.pop('groups', [])
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        user.groups.set(groups)
+        return user
+
+    def update(self, instance, validated_data):
+        groups = validated_data.pop('groups', None)
+        password = validated_data.pop('password', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+        if groups is not None:
+            instance.groups.set(groups)
+
+        instance.save()
+        return instance
+
 class ContratoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contrato
